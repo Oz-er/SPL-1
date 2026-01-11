2b61538c73391353314ed523b41ec731186a7400
Omer <omermahboob@Omers-MacBook-Air.local>
2026-01-12 04:22:04 +0600

    added main function to test output of each ingredient

--- ingredients.cpp
+++ ingredients.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include"custom_matrix.h"
 
 
 using namespace std;
@@ -58,13 +59,16 @@ vector<vector<double>> kernel(vector<vector<double>>&stk){
 }
 
 
+
+
+
 vector<vector<double>> Centering(vector<vector<double>>&kernel){
 
     int samples = kernel.size();
     vector<vector<double>>c(samples,vector<double>(samples));
 
     for(int i=0;i<samples;i++){
-        for(int j=i;j<samples;j++){
+        for(int j=0;j<samples;j++){
             
 
             if(i==j){
@@ -86,7 +90,10 @@ vector<vector<double>> Centering(vector<vector<double>>&kernel){
 
 
 
-vector<vector<double>> MMD(vector<vector<double>>&kernel ,vector<vector<double>>source, vector<vector<double>>target){
+
+
+
+vector<vector<double>> weighting_mat(vector<vector<double>>&kernel ,vector<vector<double>>&source, vector<vector<double>>&target){
 
     int samples = kernel.size();
     vector<vector<double>>l(samples,vector<double>(samples));
@@ -116,6 +123,97 @@ vector<vector<double>> MMD(vector<vector<double>>&kernel ,vector<vector<double>>
     }
 
     return l;
+}
+
+
+
+
+
+
+
+int main(){
+
+    int n=10;
+    
+    vector<vector<double>> source(n,vector<double>(n));
+    vector<vector<double>> target(n,vector<double>(n));
+
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>source[i][j];
+        }
+    }
+
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>target[i][j];
+        }
+    }
+
+    
+    auto stk=stacking(source,target); //20*10
+    auto K=kernel(stk); //(20*10).(10*20)=(20*20)
+
+    int m = K.size();
+
+    auto H = Centering(K);
+    auto tmp =matmult(H,K);
+    auto kc = matmult(tmp,H); 
+
+    auto W = weighting_mat(K,source,target);
+
+   
+
+    cout<<"Stacked matrix: "<<endl;
+    for(int i=0;i<stk.size();i++){
+        for(int j=0;j<stk[0].size();j++){
+            cout<<stk[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    cout<<endl;
+
+
+
+    cout<<"Kernel matrix: "<<endl;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<m;j++){
+            cout<<K[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    cout<<endl;
+
+
+
+    cout<<"Centering matrix: "<<endl;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<m;j++){
+            cout<<kc[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    cout<<endl;
+
+
+
+    cout<<"Weight matrix: "<<endl;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<m;j++){
+            cout<<W[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    cout<<endl;
+    
+    
+
 }
 
 
